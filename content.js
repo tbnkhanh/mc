4ce705ef87d75f024e06adb9f21437cc5a717a1f
khanhtran68 <khanh.tran@boom.tv>
2024-08-05 22:52:53 +0700

    update

--- content.js
+++ content.js
@@ -1,20 +1,13 @@
-// console.clear();
-
 let contentTitle;
 
-console.log(document.cookie);
 function dynamicClothingSection(ob) {
   let boxDiv = document.createElement("div");
   boxDiv.id = "box";
 
   let boxLink = document.createElement("a");
-  // boxLink.href = '#'
   boxLink.href = "/contentDetails.html?" + ob.id;
-  // console.log('link=>' + boxLink);
 
   let imgTag = document.createElement("img");
-  // imgTag.id = 'image1'
-  // imgTag.id = ob.photos
   imgTag.src = ob.preview;
 
   let detailsDiv = document.createElement("div");
@@ -42,50 +35,29 @@ function dynamicClothingSection(ob) {
   return boxDiv;
 }
 
-//  TO SHOW THE RENDERED CODE IN CONSOLE
-// console.log(dynamicClothingSection());
-
-// console.log(boxDiv)
-
-let mainContainer = document.getElementById("mainContainer");
 let containerClothing = document.getElementById("containerClothing");
 let containerAccessories = document.getElementById("containerAccessories");
-// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
 
-// BACKEND CALLING
-
-let httpRequest = new XMLHttpRequest();
-
-httpRequest.onreadystatechange = function() {
-  if (this.readyState === 4) {
-    if (this.status == 200) {
-      // console.log('call successful');
-      contentTitle = JSON.parse(this.responseText);
-      if (document.cookie.indexOf(",counter=") >= 0) {
-        var counter = document.cookie.split(",")[1].split("=")[1];
-        document.getElementById("badge").innerHTML = counter;
-      }
-      for (let i = 0; i < contentTitle.length; i++) {
-        if (contentTitle[i].isAccessory) {
-          console.log(contentTitle[i]);
-          containerAccessories.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        } else {
-          console.log(contentTitle[i]);
-          containerClothing.appendChild(
-            dynamicClothingSection(contentTitle[i])
-          );
-        }
-      }
-    } else {
-      console.log("call failed!");
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    contentTitle = data;
+    if (document.cookie.indexOf(",counter=") >= 0) {
+      var counter = document.cookie.split(",")[1].split("=")[1];
+      document.getElementById("badge").innerHTML = counter;
     }
-  }
-};
-httpRequest.open(
-  "GET",
-  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
-  true
-);
-httpRequest.send();
+    for (let i = 0; i < contentTitle.length; i++) {
+      if (contentTitle[i].isAccessory) {
+        containerAccessories.appendChild(
+          dynamicClothingSection(contentTitle[i])
+        );
+      } else {
+        containerClothing.appendChild(
+          dynamicClothingSection(contentTitle[i])
+        );
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Error loading the JSON data:', error);
+  });
